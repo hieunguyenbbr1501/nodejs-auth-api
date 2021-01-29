35aa42d39a7af0b0f9c48c95e3cd9dbb9cebf0dc
= <hieunguyen.bbr1501@gmail.com>
2021-01-29 17:36:54 +0700

    [feature] : Update data route

--- controller/home.js
+++ controller/home.js
@@ -19,5 +19,104 @@ module.exports = {
         }).catch((error) => {
             res.status(403).send("Password or Username is not right")
         });
+    },
+    dashboard(req, res) {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify([
+            {
+                "name": "Month To Date",
+                "value": 10000,
+                "currencyUnit": "$",
+                "important": false
+            },
+            {
+                "name": "Estimated Spend",
+                "value": 10000,
+                "currencyUnit": "$",
+                "important": false
+            },
+            {
+                "name": "Last Month",
+                "value": 9000,
+                "currencyUnit": "$",
+                "important": false
+            },
+            {
+                "name": "Change from Last Month",
+                "value": 1000,
+                "currencyUnit": "$",
+                "important": true
+            }
+        ]));
+    },
+    service(req, res) {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify([
+            {
+                "name": "EC2",
+                "price": 100.1,
+                "currency": "$",
+                "color": "yellow"
+            },
+            {
+                "name": "S3",
+                "price": 120.6,
+                "currency": "$",
+                "color": "blue"
+            },
+            {
+                "name": "EFS",
+                "price": 100.1,
+                "currency": "$",
+                "color": "red"
+            },
+            {
+                "name": "SUPPORT",
+                "price": 100.1,
+                "currency": "$",
+                "color": "green"
+            },
+            {
+                "name": "OTHER",
+                "price": 227.7,
+                "currency": "$",
+                "color": "orange"
+            }
+        ]));
+    },
+    instance(req, res) {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify([
+            {
+                "name": "T2 Medium",
+                "demand": 1021.22,
+                "reserved": 0
+            },
+            {
+                "name": "T2 Nano",
+                "demand": 2184.22,
+                "reserved": 0
+            },
+            {
+                "name": "M4.Large",
+                "demand": 1021.22,
+                "reserved": 0
+            },
+            {
+                "name": "T2 Micro",
+                "demand": 1021.22,
+                "reserved": 1000
+            },
+            {
+                "name": "M3.Medium",
+                "demand": 1021.22,
+                "reserved": 0
+            },
+            {
+                "name": "T2 Small",
+                "demand": 1021.22,
+                "reserved": 0
+            }
+        ]))
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
+const controller = require("../controller/home");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.get('/', )
-
+router.get('/cost', controller.dashboard);
+router.get('/service', controller.service);
+router.get('/instance', controller.instance);
 module.exports = router;
